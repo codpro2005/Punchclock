ba8c3a2254430051939b1e14f1633c07410a3fb7
Danilo Furrer <danilo.furrer@outlook.com>
2019-09-17 00:44:42 +0200

    added getCurrentUser method which returns the user who's username is saved in the jwt payload, made username unique and created client project with Angular

---
+++ PunchclockAPI/src/main/java/ch/zli/m223/punchclock/controller/UserController.java
@@ -0,0 +1,71 @@
+package ch.zli.m223.punchclock.controller;
+
+import ch.zli.m223.punchclock.domain.User;
+import ch.zli.m223.punchclock.service.UserService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.List;
+
+import static ch.zli.m223.punchclock.security.SecurityConstants.HEADER_STRING;
+import static ch.zli.m223.punchclock.security.SecurityConstants.TOKEN_PREFIX;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private UserService userService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserController(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @RequestMapping("/current")
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public User getCurrentUser(@Valid HttpServletRequest servlet) throws IOException, NoSuchFieldException, IllegalAccessException {
+        String jwt = servlet.getHeader(HEADER_STRING).substring(TOKEN_PREFIX.length());
+        String[] split_string = jwt.split("\\.");
+        String base64EncodedHeader = split_string[0];
+        String base64EncodedBody = split_string[1];
+        String base64EncodedSignature = split_string[2];
+
+        String payload = new String(new Base64(true).decode(base64EncodedBody));
+        JSONObject object = new JSONObject(payload);
+        String username = object.getString("sub");
+        User matchingUser = userService.findAll().stream().filter(t -> t.getUsername().equals(username)).findFirst().get();
+        return matchingUser;
+    }
+
+    @RequestMapping("/sign-up")
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@Valid @RequestBody User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        return userService.createUser(user);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteUser(@Valid @RequestBody User user) { userService.deleteUser(user); }
+}
---
+++ PunchclockAPI/src/main/java/ch/zli/m223/punchclock/domain/User.java
@@ -0,0 +1,128 @@
+package ch.zli.m223.punchclock.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private boolean isAdmin;
+
+    @JsonManagedReference
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "creator")
+    private List<Entry> entries;
+
+    @JsonManagedReference
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "creator")
+    private List<Message> messages;
+
+    @JsonManagedReference
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "creator")
+    private List<MessageReceived> messagesReceived;
+
+    @JsonManagedReference
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "creator")
+    private List<Note> notes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String email) {
+        this.username = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public List<Entry> getEntries() {
+        return entries;
+    }
+
+    public void setEntries(List<Entry> entries) {
+        this.entries = entries;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    public List<MessageReceived> getMessagesReceived() {
+        return messagesReceived;
+    }
+
+    public void setMessagesReceived(List<MessageReceived> messagesReceived) {
+        this.messagesReceived = messagesReceived;
+    }
+
+    public List<Note> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+    }
+
+//    public String toJSONString() {
+//        String decodedJSON;
+//        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+//        try {
+//            String encodedJSON = ow.writeValueAsString(this);
+//            Object o1 = JSONValue.parse(encodedJSON);
+//            JSONObject jsonObj = (JSONObject) o1;
+//            decodedJSON =
+//                    "\"id\": " + "\"" + (Long) jsonObj.get("id") + "\"" + ", " +
+//                    "\"email\": " + "\"" + (String) jsonObj.get("email") + "\"" + ", " +
+//                    "\"password\": " + "\"" + (String) jsonObj.get("password") + "\"";
+//            ;
+//        } catch (JsonProcessingException e) {
+//            decodedJSON = "";
+//        }
+//        return decodedJSON;
+//    }
+}
