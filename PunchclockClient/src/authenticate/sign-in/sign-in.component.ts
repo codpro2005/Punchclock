c59deb0a6848a0c0bbdb2a43ec88ccee37a092ad
Danilo Furrer <danilo.furrer@outlook.com>
2019-09-18 20:27:29 +0200

    Fixed bug where on empty form submit password confirm control would be valid.

--- PunchclockClient/src/authenticate/sign-in/sign-in.component.ts
+++ PunchclockClient/src/authenticate/sign-in/sign-in.component.ts
@@ -18,4 +18,7 @@ export class SignInComponent implements OnInit {
     });
   }
 
+  public onSubmit() {
+
+  }
 }
--- PunchclockClient/src/authenticate/sign-up/sign-up.component.ts
+++ PunchclockClient/src/authenticate/sign-up/sign-up.component.ts
@@ -4,7 +4,7 @@ import { ErrorStateMatcher } from '@angular/material/core';
 
 class PasswordConfirmErrorMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    return (Boolean)(control.touched && (form.hasError('differentPassword') || control.errors));
+    return (Boolean)(form.submitted || (control.touched && (form.hasError('differentPassword') || control.errors)));
   }
 }
 
